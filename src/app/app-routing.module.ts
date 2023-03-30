95e1267d8e826e8cdcdc0ec6716158cad6aba3eb
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-03-30 15:09:47 -0500

    some small adjustments
    routing to signup/signin/graphs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { GraphsComponent } from './graphs/graphs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'signin', component: SignUpComponent },
+  { path: 'signup', component: SignInComponent },
+  { path: 'graphs', component: GraphsComponent },
+  { path: '', redirectTo: '/signup', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
