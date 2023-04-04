8df1e8cfeb3a24c8bccefd0482c0001726a1ee37
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-04-04 11:57:47 -0500

    front end styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { GraphsComponent } from './graphs/graphs.component';
+import { LandingComponent } from './Elements/landing/landing.component';
 
 const routes: Routes = [
 
+  { path: 'landing', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'graphs', component: GraphsComponent },
-  { path: '', redirectTo: '/signup', pathMatch: 'full' },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
 
 ];
 
