61133d9ecda0eaffe318daf41c678d2b0585acf3
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-04-04 14:26:19 -0500

    landing and calcs layout implemented

--- src/app/elements/landing/landing.component.ts
+++ src/app/elements/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, Input, Output } from '@angular/core';
+import { Component, ElementRef, OnInit, HostListener, } from '@angular/core';
 // import { ElementCalculatorService } from 'src/app/shared/element-calculator.service';
 import { VolumeService } from 'src/app/shared/volume.service';
 
@@ -8,10 +8,22 @@ import { VolumeService } from 'src/app/shared/volume.service';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-
-  constructor() { }
-
+  isSmallViewport: boolean;
+  isMediumViewport: boolean;
+  
+  constructor() {
+    this.updateViewportSize();
+  }
   ngOnInit(): void {
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.updateViewportSize();
+  }
+
+  updateViewportSize() {
+    this.isSmallViewport = window.innerWidth <= 650;
+    this.isMediumViewport = window.innerWidth > 650 && window.innerWidth <= 1500;
+  }
 }
