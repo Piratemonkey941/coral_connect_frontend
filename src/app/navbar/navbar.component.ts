b2ccc64e9aad79bbeecf8fb8faa94448217d1bc6
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-04-08 17:44:05 -0500

    User Reg/Login/Logout implemented/tested

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-
+import { AuthService } from '.././shared/auth.service';
+import { User } from '../models/user.model';
+import { UserService } from '../shared/user.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,14 +10,29 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 })
 export class NavbarComponent implements OnInit {
 
+  currentUser: User;
   isSmallScreen: boolean;
 
-  ngOnInit(): void {
-  }
-
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor
+  (
+    private breakpointObserver: BreakpointObserver,
+    private userService: UserService,
+    private authService: AuthService
+  )
+  {
     this.checkScreenSize();
   }
+
+  ngOnInit(): void {
+    this.userService.currentUserSubject.subscribe((user: User) => {
+      console.log(user);
+      this.currentUser = user;
+    })
+}
+
+logout() {
+  this.authService.logout();
+}
 
   checkScreenSize() {
     this.breakpointObserver
