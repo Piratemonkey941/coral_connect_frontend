47fb45a5a5912a25e17ed292233524208f42e543
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-04-08 11:09:32 -0500

    Adjustment to Auth Service with added comments
    Comments generated with GPT for self edification

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -28,65 +28,87 @@ export class AuthService {
       this.currentUser = this.currentUserSubject.asObservable();
     }
 
-  signup(user: any) {
-    return this.http.post(this.baseUrl + 'create', user);
-  }
 
-  login(user: any) {
-    return this.http.post(this.baseUrl + 'login', user);
-  }
+signup(user: any) {                                       // This function sends a POST request to create a new user account with the details provided in the user object
+  return this.http.post(this.baseUrl + 'create', user);   // Send the HTTP request to the server to create a new user account
+}
 
-  setToken(token: any) {
-    localStorage.setItem('token', JSON.stringify(token));
-  }
+// This function sends a POST request to authenticate the user with the credentials provided in the user object
+login(user: any) {
+  // Send the HTTP request to the server to authenticate the user
+  // Apply the 'map' operator to modify the server's response before returning it
+  return this.http.post(this.baseUrl + 'login', user).pipe(map((response: any) => {
+    // Set the current user in the application's authentication service
+    this.currentUserSubject.next(response.user);
+    this.userService.setCurrentUser(response.user);
+    // Return the modified server's response
+    return response;
+  }));
+}
 
-  autoLogin() {
-    const token = this.getToken();
+// This function stores the token object in the browser's local storage
+setToken(token: any) {
+  localStorage.setItem('token', JSON.stringify(token));
+}
 
-    if (token == null) {
-      this.route.navigate(['/login']);
-    } else {
-      this.http.get(this.baseUrl + 'me', {
-        headers: {
-          Authorization: `Bearer ${token.value}`
-        }
-      }).subscribe((user: User) => {
+// This function automatically logs in the user if a valid token is stored in local storage
+autoLogin() {
+  const token = this.getToken();      // Get the token from local storage
+
+  if (token == null) {                // Check if a token exists
+    this.route.navigate(['/login']);  // If no token exists, navigate to the login page
+  } else {                            // If a token exists, send a GET request to the server to get the user object associated with the token
+    this.http.get(this.baseUrl + 'me', {
+      headers: {
+        Authorization: `Bearer ${token.value}`  // Set the 'Authorization' header to include the token value
+      }
+    }).subscribe((user: User) => {              // If the server returns a valid user object, set the current user in the application's authentication service
         this.userService.setCurrentUser(user);
-        this.route.navigate(['/home']);
-      });
-    }
+        this.route.navigate(['/home']);         // Navigate to the home page
+    }, (error) => {
+      // If an error occurs (e.g. the token is invalid), handle the error here, such as navigating to the login page or showing an error message.
+      console.error('Auto-login failed:', error);
+      this.route.navigate(['/login']);
+    });
   }
+}
 
-  logout() {
-    const token = this.getToken();
 
-    if (token) {
-      this.http.delete(this.baseUrl + 'logout', {
-        headers: {
-          Authorization: `Bearer ${token.value}`
-        }
-      }).subscribe((res: any) => {
-        if (res.success) {
-          this.setToken(null);
-          this.userService.setCurrentUser(null);
-          this.route.navigate(['/login']);
-        }
-      })
-    }
+logout() {
+const token = this.getToken();                // Get the token from local storage
+
+  if (token) {
+    // If a token exists, send a DELETE request to the server to invalidate the token
+    this.http.delete(this.baseUrl + 'logout', {
+      headers: {
+        Authorization: `Bearer ${token.value}` // Set the 'Authorization' header to include the token value
+      }
+    }).subscribe((res: any) => {
+      if (res.success) {
+        // If the server returns a successful response, log the user out by clearing the token and current user data and navigating to the login page
+        this.setToken(null);
+        this.userService.setCurrentUser(null);
+        this.currentUserSubject.next(null);
+        this.route.navigate(['/login']);
+      }
+    }, (error) => {
+      console.error('Logout failed:', error);   // Optionally, handle the error here, such as showing an error message to the user.
+    });
   }
+}
 
-  getToken() {
-    const token = localStorage.getItem('token');
+getToken() {
+  const token = localStorage.getItem('token');  // Get the token string from local storage
 
-    if (token != null) {
-      return JSON.parse(token);
-    } else {
-      return null;
-    }
+  if (token != null) {
+    return JSON.parse(token);                   // If a token string exists, parse it from a stringified JSON object and return it
+  } else {
+    return null;                                // If no token string exists, return null
   }
+}
 
   isLoggedIn(): boolean {
-    return !!this.currentUserValue; // Returns true if there's a user object, false otherwise
+    return !!this.currentUserValue;             // Returns true if there's a user object, false otherwise
   }
 
   getCurrentUserId(): number | null {
